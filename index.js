76dcc3509db1eff09be56e78c7aa57a9ffbfe148
Xojakbar <xojakbar@MacBook-Pro-Xojakbar.local>
2023-04-06 16:41:53 +0500

    info

---
+++ index.js
@@ -0,0 +1,20 @@
+let text =
+  " Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam libero ";
+console.log(text);
+// premitive -  string , symbol ,  number ,  null , underfine , bigin;
+//  nonpremitive - object , array , function ;
+// let name1 = "xojiakbar";
+// let a = 121;
+// let b = Symbol(12);
+// console.log(name1, a, b);
+// let b = 12;
+// a = null;
+// b = undefined;
+// console.log(a + 1);
+// console.log(b + 1);
+
+let obj = {int: 1 , 3,4 };
+let arr = [0, 1, 2, 3, 4, 5];
+let func = function () {};
+console.log(arr[3]);
+console.log(int));
