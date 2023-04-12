735dd61f9b692e9abe5c9008da794ce709777a08
xojiakbarakbarov <xakbarov508@gmail.com>
2023-04-12 15:41:04 +0500

    vazifa 11.04.2023

--- dars3/vazifa.js
+++ dars3/vazifa.js
@@ -1,3 +1,21 @@
-let a = 18856;
-for (sum = 0; a > 0; sum += a % 10, a = parseInt(a / 10)) {}
-console.log(sum);
+// ixtiyoriy kiritilgan sonni raqamlar yig'indisini topish.
+
+// let a = 373837;
+// for (sum = 0; a > 0; sum += a % 10, a = parseInt(a / 10)) {}
+// console.log(sum);
+
+// 1 dan 100 gacha bolgan barcha tub sonlarni topuvchi dastur>
+
+for (let i = 2; i <= 100; ++i) {
+  let ctr = 0;
+
+  for (let j = 2; j <= parseInt(i / 2); ++j) {
+    if (i % j == 0) {
+      ctr = 1;
+    }
+  }
+
+  if (ctr == 0) {
+    console.log(i);
+  }
+}
--- index.js
+++ index.js
@@ -23,6 +23,6 @@
 //     break;
 // }
 
-let a = 18856;
-for (sum = 0; a > 0; sum += a % 10, a = parseInt(a / 10)) {}
-console.log(sum);
+// let a = 18856;
+// for (sum = 0; a > 0; sum += a % 10, a = parseInt(a / 10)) {}
+// console.log(sum);
