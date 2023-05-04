75f8d68a1d0df2db2b6c27c527e7f742a3e4de08
xojiakbarakbarov <xakbarov508@gmail.com>
2023-05-04 14:01:41 +0500

    data types

--- 6.042023/index.js
+++ 6.042023/index.js
@@ -1,39 +1,39 @@
-// let a = 20;
-// if (a > 19) {
-//   console.log("katta");
-// } else {
-//   console.log("kichik");
-// }
+// // let a = 20;
+// // if (a > 19) {
+// //   console.log("katta");
+// // } else {
+// //   console.log("kichik");
+// // }
 
-let a = 123212;
-for (let i = a % 10; i >= 0; i = parseInt(i / 10)) {
-  switch (i) {
-    case 1:
-      console.log("bir");
-      break;
-    case 2:
-      console.log("ikki");
-      break;
-    case 3:
-      console.log("uch");
-      break;
-    case 4:
-      console.log("to'rt");
-      break;
-    case 5:
-      console.log("besh");
-      break;
-    case 6:
-      console.log("olti");
-      break;
-    case 7:
-      console.log("yetti");
-      break;
-    case 8:
-      console.log("sakkiz");
-      break;
-    case 9:
-      console.log("to'qqiz");
-      break;
-  }
-}
+// let a = 123212;
+// for (let i = a % 10; i >= 0; i = parseInt(i / 10)) {
+//   switch (i) {
+//     case 1:
+//       console.log("bir");
+//       break;
+//     case 2:
+//       console.log("ikki");
+//       break;
+//     case 3:
+//       console.log("uch");
+//       break;
+//     case 4:
+//       console.log("to'rt");
+//       break;
+//     case 5:
+//       console.log("besh");
+//       break;
+//     case 6:
+//       console.log("olti");
+//       break;
+//     case 7:
+//       console.log("yetti");
+//       break;
+//     case 8:
+//       console.log("sakkiz");
+//       break;
+//     case 9:
+//       console.log("to'qqiz");
+//       break;
+//   }
+// }
