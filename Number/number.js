8ab57fe05a2d1a7c90f97cbe0b34c033062c30dc
xojiakbarakbarov <xakbarov508@gmail.com>
2023-05-04 13:56:27 +0500

    number

--- Number/number.js
+++ Number/number.js
@@ -17,18 +17,16 @@
 
 //berilgan sonlar orasidan eng kattas va eng kichik sonlar ayirmasini toping. manfiy son chiqmasligi kerak.
 
-// let a = 10,
-//   b = 28,
-//   c = 24,
-//   d = 39,
-//   g,
-//   h;
+let a = 10,
+  b = 28,
+  c = 24,
+  d = 39,
+  g,
+  h;
 
-// h = Math.max(a, b, c, d);
-// g = Math.min(a, b, c, d);
-// console.log("eng katta son va eng kichik sonlar ayirmasini = ", h - g);
-// console.log("eng katta son va eng kichik sonlar yig'indisi = ", h + g);
+h = Math.max(a, b, c, d);
+g = Math.min(a, b, c, d);
+console.log("eng katta son va eng kichik sonlar ayirmasini = ", h - g);
+console.log("eng katta son va eng kichik sonlar yig'indisi = ", h + g);
 
-// randomInteger
-
-
+randomInteger;
